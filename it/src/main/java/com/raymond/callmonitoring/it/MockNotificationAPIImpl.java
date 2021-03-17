0eadebb60850093d629c1893f5420d81cf9a9560
raymond.wu <raymond.wu@LMXMN124.local>
2021-03-17 23:10:18 +0800

    add integration test based on junit.

--- it/src/main/java/com/raymond/callmonitoring/it/MockNotificationAPIImpl.java
+++ it/src/main/java/com/raymond/callmonitoring/it/MockNotificationAPIImpl.java
@@ -2,25 +2,30 @@ package com.raymond.callmonitoring.it;
 
 import com.raymond.callmonitoring.common.JSONUtils;
 import com.raymond.callmonitoring.server.service.NotificationAPI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class MockNotificationAPIImpl implements NotificationAPI{
-    private static final Logger logger = LoggerFactory.getLogger(MockNotificationAPIImpl.class);
-    private LinkedBlockingQueue blockingQueue = new LinkedBlockingQueue();
+public class MockNotificationAPIImpl implements NotificationAPI {
+    private CountDownLatch countDownLatch;
+    private LinkedBlockingQueue<Object> notifications;
 
     @Override
     public boolean sendNotification(Object obj) {
-        logger.info(JSONUtils.toJsonString(obj));
-        blockingQueue.add(obj);
+        countDownLatch.countDown();
+        notifications.add(obj);
         return true;
     }
 
-    public List<Object> getAllNotifications() {
-        return Arrays.asList(blockingQueue.toArray());
+    public void setCountDownLatch(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
+    public LinkedBlockingQueue<Object> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(LinkedBlockingQueue<Object> notifications) {
+        this.notifications = notifications;
     }
 }
--- it/src/test/java/com/raymond/callmonitoring/it/Test.java
+++ it/src/test/java/com/raymond/callmonitoring/it/Test.java
@@ -1,7 +1,6 @@
 package com.raymond.callmonitoring.it;
 
 import akka.actor.Props;
-import com.raymond.callmonitoring.common.JSONUtils;
 import com.raymond.callmonitoring.emulator.CallSessionBuilder;
 import com.raymond.callmonitoring.model.CallSession;
 import com.raymond.callmonitoring.model.CallSubscription;
@@ -11,8 +10,6 @@ import com.raymond.callmonitoring.server.AkkaActorSystem;
 import com.raymond.callmonitoring.server.actor.CallSubscriptionRouter;
 import com.raymond.callmonitoring.server.model.CallQueueStats;
 import com.raymond.callmonitoring.server.service.ActorService;
-import com.raymond.callmonitoring.server.service.NotificationAPI;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,13 +19,14 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Test {
 
     private static final Logger logger = LoggerFactory.getLogger(Test.class);
 
     @org.junit.Test
-    public void testInitialSubscriptionData() throws InterruptedException, MQClientException {
+    public void testInitialSubscriptionData() throws InterruptedException {
         AkkaActorSystem akkaActorSystem = AkkaActorSystem.getInstance();
         akkaActorSystem.getActorSystem().actorOf(Props.create(CallSubscriptionRouter.class), CallSubscriptionRouter.ACTOR_NAME);
 
@@ -51,28 +49,121 @@ public class Test {
         callSubscription.setQueueIdList(Arrays.asList(1L, 2L, 3L));
 
         ActorService actorService = new ActorService();
-        int expectedNotificationCount=3;
+        int expectedNotificationCount = 3;
         CountDownLatch countDownLatch = new CountDownLatch(expectedNotificationCount);
-        List<Object> notifications = new ArrayList<>();
-        NotificationAPI notificationAPI = new NotificationAPI() {
-            @Override
-            public boolean sendNotification(Object obj) {
-                notifications.add(obj);
-                countDownLatch.countDown();
-                return false;
-            }
-        };
-        actorService.createCallSubscriptionActor(notificationAPI, callSubscription, "integrationTesting");
+        LinkedBlockingQueue<Object> notifications = new LinkedBlockingQueue<>();
+        MockNotificationAPIImpl mockNotificationAPI = new MockNotificationAPIImpl();
+        mockNotificationAPI.setCountDownLatch(countDownLatch);
+        mockNotificationAPI.setNotifications(notifications);
+        actorService.createCallSubscriptionActor(mockNotificationAPI, callSubscription, "integrationTesting");
         countDownLatch.await();
 
-        CallQueueStats queue1Stats = (CallQueueStats) notifications.get(0);
+        CallQueueStats queue1Stats = (CallQueueStats) notifications.poll();
         Assert.assertEquals(queue1Stats.getQueueId().longValue(), 1);
         Assert.assertEquals(queue1Stats.getWaitingCount(), 5);
         Assert.assertTrue(queue1Stats.getLongestWaitingSeconds() >= 10000 && queue1Stats.getLongestWaitingSeconds() < 11000);
 
+        CallQueueStats queue2Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue2Stats.getQueueId().longValue(), 2);
+        Assert.assertEquals(queue2Stats.getWaitingCount(), 3);
+        Assert.assertTrue(queue2Stats.getLongestWaitingSeconds() >= 6000 && queue2Stats.getLongestWaitingSeconds() < 7000);
+
+        CallQueueStats queue3Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue3Stats.getQueueId().longValue(), 3);
+        Assert.assertEquals(queue3Stats.getWaitingCount(), 2);
+        Assert.assertTrue(queue3Stats.getLongestWaitingSeconds() >= 5000 && queue3Stats.getLongestWaitingSeconds() < 6000);
+
+        for (CallSession callSession : buildQueue2CallSessions(2L)) {
+            callProducer.produce(callSession);
+        }
+
+
     }
 
-    private static List<CallSession> buildQueue1CallSessions(Long queueId){
+    @org.junit.Test
+    public void testSubscriptionDataWhenNewCallComing() throws InterruptedException {
+        AkkaActorSystem akkaActorSystem = AkkaActorSystem.getInstance();
+        akkaActorSystem.getActorSystem().actorOf(Props.create(CallSubscriptionRouter.class), CallSubscriptionRouter.ACTOR_NAME);
+
+        CallProducer callProducer = new MockCallProducer();
+
+        List<CallSession> list = new ArrayList<>();
+        list.addAll(buildQueue1CallSessions(1L));
+        for (CallSession callSession : list) {
+            callProducer.produce(callSession);
+        }
+
+        CallConsumer callConsumer = new MockCallConsumer();
+        callConsumer.startConsuming();
+
+
+        CallSubscription callSubscription = new CallSubscription();
+        callSubscription.setUserId(1L);
+        callSubscription.setQueueIdList(Arrays.asList(1L, 2L, 3L));
+
+        ActorService actorService = new ActorService();
+        int expectedNotificationCount = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(expectedNotificationCount);
+        LinkedBlockingQueue<Object> notifications = new LinkedBlockingQueue<>();
+        MockNotificationAPIImpl mockNotificationAPI = new MockNotificationAPIImpl();
+        mockNotificationAPI.setCountDownLatch(countDownLatch);
+        mockNotificationAPI.setNotifications(notifications);
+
+        actorService.createCallSubscriptionActor(mockNotificationAPI, callSubscription, "integrationTesting");
+        countDownLatch.await();
+
+        logger.info("assert initial notifications.");
+        CallQueueStats queue1Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue1Stats.getQueueId().longValue(), 1);
+        Assert.assertEquals(queue1Stats.getWaitingCount(), 5);
+        Assert.assertTrue(queue1Stats.getLongestWaitingSeconds() >= 10000 && queue1Stats.getLongestWaitingSeconds() < 11000);
+
+        CallQueueStats queue2Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue2Stats.getQueueId().longValue(), 2);
+        Assert.assertEquals(queue2Stats.getWaitingCount(), 0);
+        Assert.assertTrue(queue2Stats.getLongestWaitingSeconds() == 0);
+
+        logger.info("assert initial notifications successfully.");
+
+
+        countDownLatch = new CountDownLatch(1);
+        notifications = new LinkedBlockingQueue<>();
+
+        mockNotificationAPI.setCountDownLatch(countDownLatch);
+        mockNotificationAPI.setNotifications(notifications);
+
+        callProducer.produce(buildQueue2CallSessions(2L).get(0));
+
+        callConsumer.startConsuming();
+        countDownLatch.await();
+
+        logger.info("assert new coming call notifications.");
+
+        queue2Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue2Stats.getQueueId().longValue(), 2);
+        Assert.assertEquals(queue2Stats.getWaitingCount(), 1);
+        Assert.assertTrue(queue2Stats.getLongestWaitingSeconds() >= 6000 && queue2Stats.getLongestWaitingSeconds() < 7000);
+
+
+        countDownLatch = new CountDownLatch(1);
+        notifications = new LinkedBlockingQueue<>();
+
+        mockNotificationAPI.setCountDownLatch(countDownLatch);
+        mockNotificationAPI.setNotifications(notifications);
+
+        callProducer.produce(buildQueue2CallSessions(2L).get(1));
+        callConsumer.startConsuming();
+        countDownLatch.await();
+
+        queue2Stats = (CallQueueStats) notifications.poll();
+        Assert.assertEquals(queue2Stats.getQueueId().longValue(), 2);
+        Assert.assertEquals(queue2Stats.getWaitingCount(), 2);
+        Assert.assertTrue(queue2Stats.getLongestWaitingSeconds() >= 6000 && queue2Stats.getLongestWaitingSeconds() < 7000);
+
+
+    }
+
+    private static List<CallSession> buildQueue1CallSessions(Long queueId) {
         CallSessionBuilder builder = new CallSessionBuilder();
         Date now = new Date();
         Date fiveSecondsAgo = new Date(now.getTime() - (1000 * 5));
@@ -95,7 +186,7 @@ public class Test {
         return list;
     }
 
-    private static List<CallSession> buildQueue2CallSessions(Long queueId){
+    private static List<CallSession> buildQueue2CallSessions(Long queueId) {
         CallSessionBuilder builder = new CallSessionBuilder();
         Date now = new Date();
         Date fiveSecondsAgo = new Date(now.getTime() - (1000 * 5));
@@ -112,7 +203,7 @@ public class Test {
         return list;
     }
 
-    private static List<CallSession> buildQueue3CallSessions(Long queueId){
+    private static List<CallSession> buildQueue3CallSessions(Long queueId) {
         CallSessionBuilder builder = new CallSessionBuilder();
         Date now = new Date();
         Date fiveSecondsAgo = new Date(now.getTime() - (1000 * 5));
